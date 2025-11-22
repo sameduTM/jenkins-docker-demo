94809a81e16c71c951bde9f0ca37794762a8f81f
Ken Wekesa <wanyamak884@gmail.com>
2025-11-22 14:06:13 +0300

    Addressed failing test

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-	return {"status": "Webhook works!"}
+    return {"status": "Webhook works!"}
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=3000)
-
+    app.run(host="0.0.0.0", port=3000)
